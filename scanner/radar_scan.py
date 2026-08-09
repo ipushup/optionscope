5e1eb906a3cd781d4488f3ec1e0f29cfcae55502
ipushup <emacaunet@gmail.com>
2026-08-09 23:48:14 +0800

    Update radar_scan.py

--- scanner/radar_scan.py
+++ scanner/radar_scan.py
@@ -42,9 +42,9 @@ WATCHLIST_US = [
     "SOFI","COIN","CEG","CCJ","TSM","AVGO","INTC","ARM","LITE","COHR","AAOI",
     "POET","CRDO","ALAB","SMTC","NBIS","IREN","APLD","CRWV",
     "VRT","EQIX","AMT","IONQ","RGTI","QBTS","QUBT",
-    "RKLB","ASTS","LUNR","SPCE","LMT",
+    "RKLB","ASTS","LUNR","SPCE","UGL","AGQ","LMT",
     "SOUN","APP","DUOL","BBAI","GEV","BE","VST","PEG","NNE","OKLO","SMR",
-    "KTOS","AVAV","JOBY","QS","PLUG","RIVN","NIO","XPEV","BABA","PDD",
+    "KTOS","AVAV","JOBY","QS","PLUG","RIVN","NIO","XPEV","BABA","AXTI","PDD",
     "FUTU","NET","TQQQ","SQQQ","WDC","GLD","NEM","F","T","LUMN","NOK","RCAT","NEE","UMAC","CIEN","PCG",
     "AAL","NKE","MSTR","IBIT","PFE","VKTX","TEM","DRNZ","ZETA","SPCX","SKHY","TER","SYM","HIMS",
 ]
